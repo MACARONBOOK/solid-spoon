51e851b1dbbe7d51d844b0f76408fc015ecb7dc1
MACARONBOOK <ec2-user@ip-172-31-92-247.ec2.internal>
2022-06-20 13:27:57 +0000

    rewrite views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,28 @@
 class UsersController < ApplicationController
+
   def index
     @users = User.page(params[:page])
+    @book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
+    @user = current_user.id
     @books = @user.books.page(params[:page])
   end
 
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to edit_user_path(@user.id)
+  end
+
+  def destroy
+
+  end
+
 end
