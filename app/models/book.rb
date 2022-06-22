5d5adbf84df74a5ebd67ceced446509e48b19a35
MACARONBOOK <ec2-user@ip-172-31-92-247.ec2.internal>
2022-06-22 00:01:24 +0000

    install rspec

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,14 +1,14 @@
 class Book < ApplicationRecord
   has_one_attached :profile_image
-  belongs_to :user
+  belongs_to :user, optional: true
 
   validates :title, presence: true
   validates :body, presence: true
 
   def get_profile_image(width, height)
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no-image.jpg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      file_path = Rails.root.join('app/assets/images/noimage.png')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.pncdg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit:[width, height]).processed
   end
